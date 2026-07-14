3744bb4a8e60c56f6aa0c344355499f95812f2e9
Pierre-Salameh <pierresalam6@gmail.com>
2026-07-14 12:34:04 +0300

    Fix Docker buildd

--- back-end/routes/web.php
+++ back-end/routes/web.php
@@ -3,19 +3,10 @@
 use Illuminate\Support\Facades\Route;
 use Illuminate\Foundation\Auth\EmailVerificationRequest;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
-
 Route::get('/', function () {
-    return view('welcome');
+    return response()->json([
+        'message' => 'Technology Cafe API is running successfully'
+    ]);
 });
 
 // عرض صفحة التحقق من البريد الإلكتروني
@@ -23,13 +14,13 @@ Route::get('/email/verify', function () {
     return view('auth.verify-email');
 })->middleware('auth')->name('verification.notice');
 
-// تحقق البريد الإلكتروني باستخدام الرابط المرسل
+// تحقق البريد الإلكتروني
 Route::get('/email/verify/{id}/{hash}', function (EmailVerificationRequest $request) {
     $request->fulfill();
     return redirect('/home');
 })->middleware(['auth', 'signed'])->name('verification.verify');
 
-// إرسال رابط تحقق جديد
+// إعادة إرسال رابط التحقق
 Route::post('/email/verification-notification', function () {
     auth()->user()->sendEmailVerificationNotification();
     return back()->with('message', 'تم إرسال رابط التحقق!');
