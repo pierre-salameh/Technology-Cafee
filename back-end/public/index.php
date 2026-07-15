ddd8eaaca6afad9813befa5fb484b923ba110f1c
Pierre-Salameh <pierresalam6@gmail.com>
2026-07-15 18:51:12 +0300

    Fix Docker build5

--- back-end/public/index.php
+++ back-end/public/index.php
@@ -1,5 +1,15 @@
-
 <?php
 
-echo "PHP IS WORKING";
-exit;
+echo "1<br>";
+
+require __DIR__.'/../vendor/autoload.php';
+
+echo "2<br>";
+
+$app = require_once __DIR__.'/../bootstrap/app.php';
+
+echo "3<br>";
+
+$app->handleRequest(Illuminate\Http\Request::capture());
+
+echo "4";
